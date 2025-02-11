ffb805ca2d5b9a30c457dbb09149348db5aa93d0
Player1717 <156854855+Player1717@users.noreply.github.com>
2025-02-12 02:54:44 +0300

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // script.js
 
+document.addEventListener('DOMContentLoaded', () => {
+    if (window.Telegram && window.Telegram.WebApp) {
+        const tg = window.Telegram.WebApp;
+
+        // Фиксация альбомной ориентации
+        tg.setOrientation("landscape"); // Новый метод для установки ориентации
+        tg.expand(); // Расширяет приложение на весь экран
+    }
+});
+
 function startReading() {
     const question = document.getElementById("question").value;
 
